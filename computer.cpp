21ebb4c0f49133674a758ba7c0217a04d641e907
ratel <920796548@qq.com>
2017-12-09 20:55:27 +0800

    chauage computer and add RAM.

--- computer.cpp
+++ computer.cpp
@@ -3,17 +3,20 @@
 #include <algorithm>
 #include <string>
 
+using namespace std;
+
 enum class Level {
 	L1=1,L2,L3,L4,L5
 };
 
-class cpu{
+class cpu {
 	
 	float voltage,rate;
 	Level level;
 	
 	public:
-	cpu (float v,float r,int l):voltage(v),rate(r),level((char)l){
+
+	cpu (float v,float r,int l):voltage(v),rate(r),level((Level)l){
 		cout<<"Make a cpu!\n";
 	};
 	~cpu (){
@@ -25,26 +28,89 @@ class cpu{
 	float getrate (){
 		return rate;
 	};
-	float getlevel (){
+	int getlevel (){
 		return (int)level;
 	};
 	void setcpu (float v,float r,int l){
 		voltage=v;
 		rate=r;
-		level=l;
+		level=(Level)l;
 	};
 	void showcpu (){
 		cout<<"My cpu voltage is "<<voltage<<"."<<endl;
 		cout<<"My cpu rate is "<<rate<<"."<<endl;	
 		cout<<"My cpu Levele is L"<<(int)level<<"."<<endl;
 	};
-}
+	void runcpu ();
+	void stopcpu ();
+};
+	void cpu::runcpu (){
+		showcpu();
+		cout<<"CPU is working!\n";
+	};
+	void cpu::stopcpu (){
+		cout<<"CPU is stoping!\n";
+	};
+
+class ram {
+	
+	int volume;
+	float rate;
+	Level ddr;
+	
+	public:
+
+	ram (int vol,float ra,int l):volume(vol),rate(ra),ddr((Level)l){
+		cout<<"Make a RAM!\n";
+	};
+	~ram(){
+		cout<<"Lose a RAM!\n";
+	};
+	void setram (int vol,float ra,int l){
+		volume=vol;
+		rate=ra;
+		ddr=(Level)l;
+	};
+	void showram (){
+		cout<<"My cpu volume is "<<volume<<"."<<endl;
+		cout<<"My cpu rate is "<<rate<<"."<<endl;	
+		cout<<"My cpu Levele is DDR"<<(int)ddr<<"."<<endl;
+	};
+	int getvolume (){
+		return volume;
+	};
+	float getrate (){
+		return rate;
+	};
+	int getddr (){
+		return (int)ddr;
+	};
+	void stopram ();
+	void runram ();
+};
+	void ram::runram (){
+		showram();
+		cout<<"RAM is working!\n";
+	};
+	void ram::stopram (){
+		cout<<"RAM is stoping!\n";
+	};
 
 
-using namespace std;
 int main()
 {
-
-  return 0;
+	float a,b;
+	int c,d;
+	cout<<"Please enter CPU parameter.\n"<<"(voltage rate Level)\n";
+	cin>>a>>b>>c;
+	cpu mycpu(a,b,c);
+	mycpu.runcpu();
+	mycpu.stopcpu();
+	cout<<"Please enter RAM parameter.\n"<<"(volume rate ddrlevel)\n";
+	cin>>d>>b>>c;
+	ram myram(d,b,c);
+	myram.runram();
+	myram.stopram();
+	return 0;
 }
 
