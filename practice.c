bfd90a2567355415b03dbf554036faf5914b317f
ratel <920796548@qq.com>
2018-01-17 22:32:34 +0800

    practice form the c

--- practice.c
+++ practice.c
@@ -1,26 +1,20 @@
 #include <stdio.h>
 #include <math.h>
+#define PI 3.1415
 
-int main ()
+
+main ()
 {
-	char ch_1,ch_2,ch_3;
-	int b_1,b_2;
-	printf("Please enter a char.\n");
-	ch_1=getchar();
-	getchar();
-	printf("Please enter a char again.\n");
-	scanf("%c\n",&ch_2);
-	b_1=ch_1>='A'&&ch_1<='Z'||ch_1>='a'&&ch_1<='z';
-	b_2=ch_2>='A'&&ch_2<='Z'||ch_2>='a'&&ch_2<='z';
-	b_1?putchar('Y'):putchar('N');
-	putchar('\n');
-	ch_3=b_2?'Y':'N';
-	printf("%c\n",ch_3);
+	long a,b;
+	b=0;
+	for(a=getchar();a!=EOF;a=getchar())
 	{
-		printf("%d\n",ch_1);
-		printf("%o\n",ch_1);
-		printf("%x\n",ch_1);
-		printf("%u\n",ch_1);
+		if(a=='\t')
+			printf("\\t");
+		if(a=='\b')
+			printf("\\b");
+		if(a!='\t'&&a!='\b')
+			putchar(a);
 	}
-	return 0;
+	printf("%ld",b);
 }
