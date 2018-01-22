375aa5efa134e9ceacf1e8185b4e49c496b12a6a
ratel <920796548@qq.com>
2018-01-22 21:37:16 +0800

    add Number of char and chanage temperature(F->C)

--- test.c
+++ test.c
@@ -4,14 +4,5 @@ main ()
 {
 	int n[20],y;
 	int control;
-	y=0;
-	for(control=0;control<20;++control)
-	{
-		scanf("%d",&n[control]);
-	}
-	for(control=0;control<20;++control)
-		if(n[control]>y)
-			y=control;
-	printf("%d ",y);
-	
+	printf("%d",'0');
 }
