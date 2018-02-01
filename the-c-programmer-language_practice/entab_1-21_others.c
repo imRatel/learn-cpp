8c87c52393aec54bb000dc88dcccf0599625f1cd
ratel <920796548@qq.com>
2018-02-01 17:39:47 +0800

    add entab others

---
+++ the-c-programmer-language_practice/entab_1-21_others.c
@@ -0,0 +1,23 @@
+/*************************************************************************
+    > File Name: entab_1-21_others.c
+    > Author: ma6174
+    > Mail: ma6174@163.com 
+    > Created Time: Thu 01 Feb 2018 10:55:31 AM DST
+ ************************************************************************/
+
+#include<stdio.h>
+
+int main ()
+{
+	int i,a;
+	a = 0;
+	while (a < 10)
+	{
+		for (i = 0;i < 10; ++i)
+		{
+			printf("%d",i);
+		}
+	}
+	getchar();
+	return 0;
+}
