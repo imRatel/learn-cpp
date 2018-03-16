fa6d7166217db50b85a88bb0579f0246728676f4
kai zhou <920796548@qq.com>
2018-03-16 16:44:29 +0800

    练习5-5

---
+++ the-c-programmer-language_practice/windows/strncpy_strncat_strncmp_5-5/strncpy_strncat_strncmp_5-5/strncat.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+
+void strncat(char *s1, char *s2, int n)
+{
+	int s1_len = strlen(s1);
+	int n1, n2;
+	n1 = n2 = 0;
+
+	if (n < strlen(s1))
+	{
+		return;
+	}
+
+	while (n2 < n)
+	{
+
+	}
+}
